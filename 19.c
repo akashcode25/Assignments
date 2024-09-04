6d55041f5f257315787fdc7672a33035d9d8f99b
Akash Kumar <akashpersonal25@gmail.com>
2024-09-04 09:23:17 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char z;
+    printf("Enter any character =\n");
+    scanf("%c",&z);
+    (z>=97&&z<=122)?
+    printf("Character is in lowercase\n"):
+    printf("character is not in lowercase\n");
+}
