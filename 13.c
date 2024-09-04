82dcfd5ff63068a032a9de36649bedfc9fcd5fd1
Akash Kumar <akashpersonal25@gmail.com>
2024-09-04 09:15:17 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the integer value=");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("Even Number");
+    }
+    else
+    {
+    printf("Odd Number");
+    }
+}
