3cc3354c9a6a99809b8def4a8d0e37dff2eced88
Akash Kumar <akashpersonal25@gmail.com>
2024-09-04 09:03:32 +0530

    Create 03.c

---
+++ 03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("Basic Salary = ");
+scanf("%d",&a);
+printf("House Rent Allowance = ");
+scanf("%d",&b);
+printf("Dearness allowance = ");
+scanf("%d",&c);
+printf("Medical allowance = ");
+scanf("%d",&d);
+e=(a+b+c+d);
+printf("Gross Salary = %d", e);
+}
