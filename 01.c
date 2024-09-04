107e4c208e324b6a6b872461b93ad3e5e8f21e9b
Akash Kumar <akashpersonal25@gmail.com>
2024-09-04 08:36:24 +0530

    01.c

---
+++ 01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the amount = ");
+scanf("%d",&a);
+printf("enter the rate = ");
+scanf("%d",&b);
+printf("enter the time = ");
+scanf("%d",&c);
+d=(a*b*c)/100;
+printf("simple interest is = %d",d);
+}
